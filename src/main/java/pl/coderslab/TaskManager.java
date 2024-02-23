9f2249fd814c50259cf293185d82e8064b21775a
PiotrWiecek1995 <wiecek-piotr@o2.pl>
2024-02-23 10:31:29 +0100

    1 push

---
+++ src/main/java/pl/coderslab/TaskManager.java
@@ -0,0 +1,10 @@
+package pl.coderslab;
+
+
+
+public class TaskManager {
+    public static void main(String[] args) {
+        System.out.println("test");
+        System.out.println("Plik");
+    }
+}
